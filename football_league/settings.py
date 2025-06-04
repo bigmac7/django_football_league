49b4c434bdcda9061873ed14a99a1e2debd66587
Maksym Charuta <mcharuta@jaguarlandrover.com>
2025-06-04 13:56:07 +0100

    initial tailwind setup (tailwind init failing, no npm on machine)

--- football_league/settings.py
+++ football_league/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'league'
+    'tailwind'
+    'theme'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TAILWIND_APP_NAME = 'theme'
+NPM_BIN_PATH = "npm.cmd"

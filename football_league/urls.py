126fb27cbfaddcf0adc635efc43fdc9a18f516e7
Maksym Charuta <maxcharuta@gmail.com>
2025-06-05 13:20:59 +0100

    updated for premier league styling

--- football_league/urls.py
+++ football_league/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from league import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 app_name = 'league'
 

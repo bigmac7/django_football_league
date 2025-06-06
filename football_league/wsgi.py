26f3ff04eb0e19d9382c87a9cc025ee424093a53
Maksym Charuta <maxcharuta@gmail.com>
2025-06-06 13:51:45 +0100

    adding dist/ for pythonanywhere

--- football_league/wsgi.py
+++ football_league/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'football_league.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_football_league.settings')
 
 application = get_wsgi_application()
